cfaca58fa675131237635d492dc2c4a186a7e46c
Karthik Raju <karthikraju391@gmail.com>
2023-04-21 23:26:03 +0530

    made contact page responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,15 @@
-import { AddressBook, ArrowRight, ChartLine } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({ screenSize }: { screenSize: string }) => {
+const Navbar = () => {
 	return (
-		<nav className="md:block px-5 bg-slate-200">
-			<ul className="mt-14 flex flex-col gap-3">
-				<Link
-					to="/"
-					className="group text-2xl font-semibold flex items-center justify-center md:justify-normal cursor-pointer"
-				>
-					{screenSize === "small" ? (
-						<AddressBook size={35}/>
-					) : (
-						<>
-							Contacts
-							<ArrowRight
-								size={25}
-								className="ml-auto group-hover:translate-x-1 duration-75"
-							/>
-						</>
-					)}
-				</Link>
-				<Link
-					to="/data"
-					className="group text-2xl font-semibold flex justify-center md:justify-normal items-center"
-				>
-					{screenSize === "small" ? (
-						<ChartLine size={35}/>
-					) : (
-						<>
-							Data	
-							<ArrowRight
-								size={25}
-								className="ml-auto group-hover:translate-x-1 duration-75"
-							/>
-						</>
-					)}
-				</Link>
+		<nav className="border">
+			<ul>
+				<li>
+					<Link to="/">Contacts</Link>
+				</li>
+				<li>
+					<Link to="/data">Data</Link>
+				</li>
 			</ul>
 		</nav>
 	);
--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,26 +1,19 @@
-import { useAppSelector } from "../app/hooks";
+import { useAppSelector, useAppDispatch } from "../app/hooks";
 import NoContacts from "../components/NoContacts";
 import Contact from "../components/Contact";
 import AddContactButton from "../components/AddContactButton";
-import { PlusCircle } from "@phosphor-icons/react";
-import { useNavigate } from "react-router";
 
-const Contacts = ({ screenSize }: { screenSize: string }) => {
+const Contacts = () => {
 	const contacts = useAppSelector((state) => state.contact.contacts);
-	const navigate = useNavigate();
 
 	return (
 		<div className="flex flex-col items-start mt-14 w-3/4 mx-auto">
 			<div className=" w-full flex justify-between items-center">
-				<h1 className="text-4xl md:text-5xl font-bold">Contacts</h1>
-				{screenSize === "small" ? (
-					<PlusCircle size={35} onClick={() => navigate("/add")} />
-				) : (
-					<AddContactButton />
-				)}
+				<h1 className="text-5xl font-bold">Contacts</h1>
+				<AddContactButton />
 			</div>
 			{!contacts || (contacts.length === 0 && <NoContacts />)}
-			<ul className="w-full grid lg:grid-cols-2 mt-4 md:gap-x-4 gap-y-2">
+			<ul className="w-full grid grid-cols-2 mt-4 gap-x-4">
 				{contacts.map((contact) => (
 					<Contact key={contact.id} contact={contact} />
 				))}
